0a719c091b3a073f0c3977419077c023b0a106c4
Enzo <enzostrobino@gmail.com>
2026-07-15 15:24:19 -0300

    Enable Android core library desugaring for release builds
    flutter_local_notifications' AndroidScheduleMode.inexactAllowWhileIdle
    path needs java.time APIs that aren't available on older Android
    without desugaring turned on.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,6 +21,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
@@ -61,4 +62,8 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
